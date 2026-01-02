e78066e34b4d114625016f1a0d5174828f550155
nasti <anastasiasaienko634@gmail.com>
2026-01-02 23:11:17 +0100

    fixed Register Modal

--- src/components/Register/Register.tsx
+++ src/components/Register/Register.tsx
@@ -1,5 +1,6 @@
 import css from "./Register.module.css";
 import { Formik, Form, Field, ErrorMessage } from "formik";
+import { useEffect } from "react";
 import { IoIosClose } from "react-icons/io";
 import * as Yup from "yup";
 
@@ -9,20 +10,21 @@ interface RegisterProp {
 }
 
 interface Formikvalue {
+  name: string;
   email: string;
   password: string;
 }
 const Register = ({ isOpenRegister, toggelRegister }: RegisterProp) => {
   const SignupSchema = Yup.object().shape({
     name: Yup.string()
-      .min(2, "Too Short!")
-      .max(50, "Too Long!")
-      .required("Required"),
+      .min(2, "Name is too Short!")
+      .max(50, "Name is too Long!")
+      .required("Name is required"),
     password: Yup.string()
-      .min(2, "Too Short!")
-      .max(50, "Too Long!")
-      .required("Required"),
-    email: Yup.string().email("Invalid email").required("Required"),
+      .min(2, "Password is too Short!")
+      .max(50, "Password is too Long!")
+      .required("Password is required"),
+    email: Yup.string().email("Invalid email").required("Email is required"),
   });
 
   const handleSubmit = (values: Formikvalue) => {
@@ -30,16 +32,37 @@ const Register = ({ isOpenRegister, toggelRegister }: RegisterProp) => {
     console.log(values.password);
     toggelRegister();
   };
+
+  // Close the modal when clicking on the modal overlay
+  const handleOverlayClick = (event: React.MouseEvent<HTMLDivElement>) => {
+    if (event.target === event.currentTarget) {
+      toggelRegister();
+    }
+  };
+
+  // Close the modal when pushing Escape
+  useEffect(() => {
+    const handleKeyboardClick = (e: KeyboardEvent) => {
+      if (e.key === "Escape") {
+        toggelRegister();
+      }
+    };
+    document.addEventListener("keydown", handleKeyboardClick);
+
+    return () => {
+      document.removeEventListener("keydown", handleKeyboardClick);
+    };
+  }, [toggelRegister]);
   return (
     <>
       {isOpenRegister && (
-        <div className={css.modalOverlay}>
+        <div className={css.modalOverlay} onClick={handleOverlayClick}>
           <div className={css.modal}>
             <button onClick={toggelRegister} className={css.closeBtn}>
               <IoIosClose className={css.closeIcon} />
             </button>
-            <h1 className={css.logInTitle}>Registration</h1>
-            <p className={css.loginDescription}>
+            <h1 className={css.registerTitle}>Registration</h1>
+            <p className={css.registerDescription}>
               Thank you for your interest in our platform! In order to register,
               we need some information. Please provide us with the following
               information
@@ -49,41 +72,48 @@ const Register = ({ isOpenRegister, toggelRegister }: RegisterProp) => {
               initialValues={{ name: "", email: "", password: "" }}
               onSubmit={handleSubmit}
             >
-              <Form className={css.logInForm}>
-                <Field
-                  className={css.emailField}
-                  name="name"
-                  placeholder="Name"
-                />
-                <ErrorMessage
-                  name="name"
-                  component="span"
-                  className={css.schemaValidation}
-                />
-                <Field
-                  className={css.emailField}
-                  name="email"
-                  placeholder="Email"
-                />
-                <ErrorMessage
-                  name="email"
-                  component="span"
-                  className={css.schemaValidation}
-                />
-                <Field
-                  className={css.passwordField}
-                  name="password"
-                  placeholder="Password"
-                />
-                <ErrorMessage
-                  name="password"
-                  component="span"
-                  className={css.schemaValidation}
-                />
-                <button type="submit" className={css.loginSubmit}>
-                  Sign Up
-                </button>
-              </Form>
+              {({ isSubmitting }) => (
+                <Form className={css.registerForm}>
+                  <Field
+                    className={css.nameField}
+                    name="name"
+                    placeholder="Name"
+                  />
+                  <ErrorMessage
+                    name="name"
+                    component="span"
+                    className={css.schemaValidation}
+                  />
+                  <Field
+                    className={css.emailField}
+                    name="email"
+                    placeholder="Email"
+                  />
+                  <ErrorMessage
+                    name="email"
+                    component="span"
+                    className={css.schemaValidation}
+                  />
+                  <Field
+                    className={css.passwordField}
+                    type="password"
+                    name="password"
+                    placeholder="Password"
+                  />
+                  <ErrorMessage
+                    name="password"
+                    component="span"
+                    className={css.schemaValidation}
+                  />
+                  <button
+                    type="submit"
+                    className={css.registerSubmit}
+                    disabled={isSubmitting}
+                  >
+                    {isSubmitting ? "Loading..." : "Sign Up"}
+                  </button>
+                </Form>
+              )}
             </Formik>
           </div>
         </div>
