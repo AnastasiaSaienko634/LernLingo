e2e9a7615579dd362c4142e60777a65385b1aeff
nasti <anastasiasaienko634@gmail.com>
2026-04-08 19:44:56 +0200

    Database was add

---
+++ src/components/TeacherItem/TeacherItem.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TeacherItem = () => {
+  return <div></div>;
+};
+
+export default TeacherItem;
--- src/pages/Teachers/Teachers.tsx
+++ src/pages/Teachers/Teachers.tsx
@@ -1,7 +1,24 @@
 import React from "react";
 import Header from "../../components/Header/Header";
+import { getDatabase, ref, get } from "firebase/database";
 
 const Teachers = () => {
+  // Connect to Database
+  const db = getDatabase();
+
+  async function getAllData() {
+    // Object with data
+    const snapshot = await get(ref(db, "/"));
+
+    if (snapshot.exists()) {
+      console.log(snapshot.val());
+    } else {
+      console.log("No data");
+    }
+  }
+
+  getAllData();
+
   return (
     <>
       <Header />
