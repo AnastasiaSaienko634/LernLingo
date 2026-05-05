6284c7023f8c32e6fbd4623f1c5a96ec94e07a75
nasti <anastasiasaienko634@gmail.com>
2026-05-05 20:07:13 +0200

    add meta data

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,11 +1,18 @@
 import Header from "../../components/Header/Header";
 import Hero from "../../components/Hero/Hero";
+import MetaTags from "../../components/MetaTags/MetaTags";
+import img from "../../../public/heroPhoto.png";
 
 const Home = () => {
   return (
     <>
       <Header />
       <Hero />
+      <MetaTags
+        title="Home Page"
+        description="LernLingo helps people find the best teacher for learning languages based on their needs and goals. It connects learners with qualified instructors and makes the learning process more effective and personalized."
+        image={img}
+      />
     </>
   );
 };
